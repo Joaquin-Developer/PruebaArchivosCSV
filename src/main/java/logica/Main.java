63d7ecbc6f08fbe773a9def49fadc35052d05861
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-10-11 16:29:27 -0300

    refactorizo codigo

--- src/main/java/logica/Main.java
+++ src/main/java/logica/Main.java
@@ -11,33 +11,26 @@ public class Main {
     public static void main(String[] args) {
         
         Reader reader = new Reader();
-        ArrayList<Person> persons = new ArrayList<Person>();
+        
         try {
             String ruta = JOptionPane.showInputDialog(null, "Ingresar ruta:", "Mensaje", JOptionPane.QUESTION_MESSAGE);
         
-            ArrayList<String> textoArchivo = reader.readFile(ruta);
+            ArrayList<Person> personsList = reader.toPersonArrayObject(ruta);
             
-            // recorrer contenido, cortar texto y llenar arrayList con objetos person:
-            
-            for (String linea : textoArchivo) {
-                Person person = new Person();
-                String campos[] = linea.split(":");
-                person.setNombre(campos[0]);
-                person.setApellido(campos[1]);
-                person.setEdad(Integer.valueOf(campos[2]));
-                persons.add(person);
+            String lista = "";
+            for (Person person : personsList) {
+                lista += (person.toString() + "\n");
             }
             
-            String listaPersonas = "";
-            for (Person person : persons) {
-                listaPersonas += (person.toString() + "\n");
-            }
+            JOptionPane.showMessageDialog(null, lista, "Contenido del archivo:", JOptionPane.INFORMATION_MESSAGE);
             
-            JOptionPane.showMessageDialog(null, listaPersonas, "Contenido del archivo:", JOptionPane.INFORMATION_MESSAGE);
+        } catch (NullPointerException npEx) {
+            npEx.printStackTrace();
+            JOptionPane.showMessageDialog(null, npEx.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Se produjo un error", JOptionPane.ERROR_MESSAGE);
         }
         
     }
--- src/main/java/logica/Reader.java
+++ src/main/java/logica/Reader.java
@@ -12,17 +12,29 @@ public class Reader {
     
     public Reader() { }
     
-    public ArrayList<String> readFile(String ruta) throws FileNotFoundException, IOException {
+    public ArrayList<Person> toPersonArrayObject(String ruta) throws FileNotFoundException, 
+            IOException, NullPointerException {
         
-        ArrayList<String> texto = new ArrayList<String>(); 
+        ArrayList<Person> persons = new ArrayList<Person>();
         FileReader file = new FileReader(ruta);
         BufferedReader buffered = new BufferedReader(file);
         String linea;
         
-        while((linea = buffered.readLine()) != null) 
-            texto.add(linea);
+        // recorrer contenido, cortar texto y llenar arrayList con objetos person:
+        while((linea = buffered.readLine()) != null) {
+            Person person = new Person();
+            String campos[] = linea.split(":");
+            if (campos.length < 3) {
+                throw new NullPointerException("La línea '" + linea + "' no se cargará. Hay datos incompletos.");
+            } else {
+                person.setNombre(campos[0]);
+                person.setApellido(campos[1]);
+                person.setEdad(Integer.valueOf(campos[2]));
+                persons.add(person);
+            }
+        }
         
-        return texto;
+        return persons;
         
     }
     
